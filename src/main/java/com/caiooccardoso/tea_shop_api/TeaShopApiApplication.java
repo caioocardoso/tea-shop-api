2de1c48283b60f0155be1fa13e9706a991577a6f
caioocardoso <caiooliveiracardosoo@gmail.com>
2026-06-21 15:56:20 -0300

    first commit

---
+++ src/main/java/com/caiooccardoso/tea_shop_api/TeaShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.caiooccardoso.tea_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaShopApiApplication.class, args);
+	}
+
+}
